389707e07ddb01a703c19bb3d530050f8a38d88e
Lalit Rawool <lrrawool2503@gmail.com>
2024-01-09 22:37:04 +0530

    Add message to users functions - customers def
    9 jan 2024 1 st commit after deploy on git

--- users/views.py
+++ users/views.py
@@ -7,16 +7,17 @@ from users.accessibility import gen_api_key
 def customers(request):
     apikey = gen_api_key()
     email = request.GET.get('email')
+    #checking for existing api key
     while True:
         server_api = Customers.objects.filter(apikey=apikey)
         if(len(server_api)==0):
             break
         else:
             apikey=gen_api_key()
-    
+    #checking for conflit user email and return message
     server_api = Customers.objects.filter(email=email)
     if(len(server_api)!=0):
-        return JsonResponse({'message':'409'})
+        return JsonResponse({'status':'409','message':'User already exist..Please Login!'})
     print(len(apikey))
     print("api" ,apikey)
     name = request.GET.get('name')
@@ -26,8 +27,8 @@ def customers(request):
     birthdate = request.GET.get('birthdate')
     
     password = request.GET.get('password')
-    print(name,email,phone,address,birthdate,apikey,password)
+    # print(name,email,phone,address,birthdate,apikey,password)
     customer_obj = Customers(name=name,email=email,phone=phone,address=address,birthdate=birthdate,apikey=apikey,password=password)
     customer_obj.save()
     
-    return JsonResponse({'message':'true'})
+    return JsonResponse({'status':'200','message':'Users account created successful!'})
