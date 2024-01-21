63368e47f9739b1059a06d974220485d206a2d3c
Lalit Rawool <lrrawool2503@gmail.com>
2024-01-21 13:51:51 +0530

    major change in selller api
    api build successfully seller can register

--- seller/models.py
+++ seller/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Seller(models.Model):
-    bussinessname = models.CharField(max_length=255)
+    bussinessname = models.CharField(max_length=255,null=True,blank=True)
     uniquekey = models.CharField(max_length=255)
     bussinessemail = models.EmailField(default='abc@gmail.com')
     phone = models.CharField(max_length= 10)
@@ -12,15 +12,18 @@ class Seller(models.Model):
     district = models.CharField(max_length=50)
     taluka = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=6,default=416602)
 
     owneremail = models.EmailField(default ='abc@gmail.com')
     ownerphone = models.CharField(max_length=10)
-    ownername = models.CharField(max_length=255)
+    ownername = models.CharField(max_length=255,null=True,blank=True)
 
     bankname = models.CharField(max_length=255)
     accno = models.CharField(max_length = 50)
     ifsc = models.CharField(max_length = 25)
     aadharcard = models.CharField(max_length = 20)
+    
+    password = models.CharField(max_length=50,default='password')
 
 
 
--- seller/views.py
+++ seller/views.py
@@ -8,5 +8,53 @@ from seller.models import Seller
 def seller(request):
     seller = Seller.objects.all()
     seller_info = SellerShowSerializer(seller,many=True)
-    
+    print(request.method)
+    if request.method=='POST':
+        bussinessname = request.POST.get('bussinessname')
+        uniquekey = 'unique'
+        bussinessemail = request.POST.get('bussinessemail')
+        phone = request.POST.get('phone')
+        gst = request.POST.get('gst')
+        bussimage =request.FILES['bussimage']
+
+        state =request.POST.get('state')
+        district =request.POST.get('district')
+        taluka = request.POST.get('taluka')
+        city = request.POST.get('city')
+        pincode = request.POST.get('pin')
+
+        owneremail = request.POST.get('owneremail')
+        ownerphone = request.POST.get('ownerphone')
+        ownername = request.POST.get('ownername')
+
+        bankname = request.POST.get('bankname')
+        accno = request.POST.get('accno')
+        ifsc = request.POST.get('ifsc')
+        aadharcard = request.POST.get('aadharcard')
+        password = request.POST.get('password')
+        print(bussimage)
+        seller = Seller(bussinessname=bussinessname,uniquekey=uniquekey,bussinessemail=bussinessemail,phone=phone,gst=gst,bussimage=bussimage,state=state,district=district,taluka=taluka,city=city,pincode=pincode,owneremail=owneremail,ownerphone=ownerphone,ownername=ownername,bankname=bankname,accno=accno,ifsc=ifsc,aadharcard=aadharcard,password=password)
+        seller.save()
+        context = {
+            'bussinessname':bussinessname,
+            'uniquekey':uniquekey,
+            'bussinessemail':bussinessemail,
+            'phone':phone,
+            'gst':gst,
+            
+            'state':state,
+            'district':district,
+            'taluka':taluka,
+            'city':city,
+            'owneremail':owneremail,
+            'ownerphone':ownerphone,
+            'ownername':ownername,
+            'bankname':bankname,
+            'accno':accno,
+            'ifsc':ifsc,
+            'aadharcard':aadharcard
+           
+        }
+        return JsonResponse({'status':200,'context':context})
     return JsonResponse(seller_info.data,safe=False)
+
