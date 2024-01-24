83988a4afec9829f5d7362f0294f07cb797f4b3a
Lalit Rawool <lrrawool2503@gmail.com>
2024-01-24 20:30:15 +0530

    Change in Product model
    width,length,height, added and unique id changes to random.rantint

--- products/views.py
+++ products/views.py
@@ -4,7 +4,10 @@ from seller.models import Seller
 from users.accessibility import gen_api_key
 from products.models import Products
 from products.serializers import ProductsViewSerializer
+from django.views.decorators.csrf import csrf_exempt
+import random
 
+@csrf_exempt
 def addproduct(request):
     sellerid = request.POST.get('sellerid')
     print(sellerid)
@@ -20,34 +23,37 @@ def addproduct(request):
         seller_district = x.district
         sellerr_email = x.bussinessemail
         break
-    uniqueid = gen_api_key()
+    uniqueid = random.randint(11111,99999)
     while True:
         server_key = Products.objects.filter(uniqueid=uniqueid)
         if(len(server_key)==0):
             break
         else:
-            uniqueid = gen_api_key()
+            uniqueid = random.randint(11111,99999)
     name = request.POST.get('name')
     descrription = request.POST.get('describe')
     price = request.POST.get('price')
     delivertcharge = request.POST.get('delivertcharge')
-    
+    width = request.POST.get('width')
+    height = request.POST.get('height')
+    length = request.POST.get('length')
     category = request.POST.get('category')
-    image1 = request.POST.get('image1')
-    image2 = request.POST.get('image2')
-    image3 = request.POST.get('image3')
-    image4 = request.POST.get('image4')
-    image5 = request.POST.get('image5')
+    image1 = request.FILES['image1']
+    image2 =request.FILES['image2']
+    image3 = request.FILES['image3']
+    image4 = request.FILES['image4']
+    image5 = request.FILES['image5']
+    print(image1,image2,image3,image4,image5)
     try:
-        seller_obj = Seller(uniqueid= uniqueid,name=name, price=price, delivertcharge=delivertcharge,sellerid=sellerid,sellername=seller_name,state=seller_state,district=seller_district,selleremail=sellerr_email, category=category,image1=image1,image2=image2,image3=image3,image4=image4, image5=image5)
+        seller_obj = Products(uniqueid= uniqueid,name=name, price=price, description=descrription,delivertcharge=delivertcharge,width=width,height=height,length=length,sellerid=sellerid,sellername=seller_name,state=seller_state,district=seller_district,selleremail=sellerr_email, category=category,image1=image1,image2=image2,image3=image3,image4=image4, image5=image5)
         seller_obj.save()
-
+        return JsonResponse({'status':'200','message':'product added successful'})
     except Exception as e:
         print(e)
         
     return JsonResponse({'key':'done'})
 
-
+@csrf_exempt
 def allproduct(request):
     product = Products.objects.all()
     prod_serial = ProductsViewSerializer(product,many=True)
