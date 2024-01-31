f7746e199f521bb0df26d3af393ceb551b3895a0
Lalit Rawool <lrrawool2503@gmail.com>
2024-01-31 20:52:48 +0530

    dimention change to FloatField in model of product

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from products.models import Products
+from products.models import Products,Review
 
 class DisplayProducts(admin.ModelAdmin):
-    list_display = ('uniqueid' ,'name','description' ,'price','delivertcharge','width','height','length','sellerid','sellername' ,'state' ,'district', 'selleremail' ,'category', 'image1','image2', 'image3' ,'image4','image5')
+    list_display = ('uniqueid' ,'name','description' ,'price','delivertcharge','width','height','length','sellerid','sellername' ,'state' ,'district', 'selleremail' ,'category', 'image1','image2', 'image3' ,'image4','image5','rating','len_review')
 
+class DisplayReview(admin.ModelAdmin):
+    list_display= ('uid','productname','productid','reviewerid','reviwername','review','star','title')
+    
 admin.site.register(Products,DisplayProducts)
+admin.site.register(Review,DisplayReview)
